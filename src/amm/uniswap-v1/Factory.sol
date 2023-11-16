06fa274d7207d3c4ca4af1bd55d21b825a2ca852
trinhvanvinh <trinhvanvinh018>
2023-11-16 22:41:48 +0700

    create exchange

--- src/amm/uniswap-v1/Factory.sol
+++ src/amm/uniswap-v1/Factory.sol
@@ -1,2 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+import "./Exchange.sol";
+
+contract Factory {
+    mapping(address => address) public exchanges;
+
+    function createExchange(
+        address tokenAddress
+    ) public returns (address exchangeAddress) {
+        require(tokenAddress != address(0), "Token address not valid");
+        require(
+            exchanges[tokenAddress] == address(0),
+            "Exchange already exists"
+        );
+
+        Exchange exchange = new Exchange(tokenAddress);
+        exchanges[tokenAddress] = address(exchange);
+        exchangeAddress = address(exchange);
+    }
+}
